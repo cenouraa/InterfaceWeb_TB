63682046fd9667cc8cb1906356835ef1011779c5
matt <matheus.gonzaga186@gmail.com>
2025-06-25 19:48:42 -0300

    Atualizando o repositório da disciplina

---
+++ 03 - JS/04 - Arrays e Objetos/array09.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3]
+
+show(1,2,3)
+show(arr)
+show(...arr)//destrinchado
+
+function show(){
+    console.log(arguments)
+    console.log(arguments.length)
+}
+
+let [n1] = arr
+console.log(n1)
+let[n2,n3] = arr
+console.log(n2)
+console.log(n3)
